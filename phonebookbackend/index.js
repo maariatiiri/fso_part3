6e2358d4c7b59c1c50b7493c00ea4d8b753a429f
Maaria Tiiri <maaria.tiiri@aalto.fi>
2026-07-22 16:06:23 +0300

    3.8. done

--- phonebookbackend/index.js
+++ phonebookbackend/index.js
@@ -2,9 +2,15 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
+morgan.token('body', function (req, res) { 
+    if (req.method==='POST') {
+        return JSON.stringify(req.body) 
+    }
+    else return ' '
+})
 
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
     { 
