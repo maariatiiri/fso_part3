273fb6cfc7794605a8136d790317f00300da4a89
Maaria Tiiri <maaria.tiiri@aalto.fi>
2026-07-23 14:10:47 +0300

    3.11 done

--- phonebook/backend/index.js
+++ phonebook/backend/index.js
@@ -10,6 +10,7 @@ morgan.token('body', function (req, res) {
 })
 
 app.use(express.static('dist'))
+app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
@@ -34,10 +35,6 @@ let persons = [
       "number": "39-23-6423122"
     }
 ]
-
-app.get('/', (request, response) => {
-  response.json("Wollo Herld")
-})
 
 app.get('/api/persons', (request, response) => {
   response.json(persons)
